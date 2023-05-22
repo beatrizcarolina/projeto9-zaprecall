14accde8130176378c310cd09105fee4bcbfd63d
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-05-22 11:57:37 -0300

    fix App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import React from "react";
+import styled from "styled-components";
 import Header from "./components/Header/Header";
-
-import { useState } from "react"
+import Deck from "./components/Deck/Deck";
+import Footer from "./components/Footer/Footer"
+import cards from "./cards"
 
 export default function App() {
+    const [answers, setAnswers] = React.useState([]);
+
+    function addAnswer(result) {
+        setAnswers([...answers, result]);
+    }
 
     return (
-        <>
+        <Container>
             <Header/>
-        </>
+            <Deck handleClick={addAnswer} flashcards={cards}></Deck>
+            <Footer answers={answers} flashcards={cards}></Footer>
+        </Container>
     );
-
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+`;
