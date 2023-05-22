129debdff9d4fc439029e8eb67ddb041515f1fcd
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-05-22 11:57:03 -0300

    fix main

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import { Reset, GlobalStyle } from './globalStyles.js'
+import { GlobalStyle } from './globalStyles.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Reset/>
     <GlobalStyle/>
     <App />
   </React.StrictMode>,
